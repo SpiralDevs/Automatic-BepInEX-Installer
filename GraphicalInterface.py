ab075f786ff80518d5f85b4ee6dec770a492c2ce
Spiral <burlingamenick190@gmail.com>
2024-09-15 20:19:18 -0400

    dropdown to change what version to install

--- GraphicalInterface.py
+++ GraphicalInterface.py
@@ -1,6 +1,6 @@
 import platform
 import os
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, ttk, StringVar
 from tkinter import *
 from pygame import mixer
 try:
@@ -57,7 +57,7 @@ class InterfaceMenu:
         self.root = Tk()
         mixer.init()
         self.root.configure(background="#353738")
-        self.root.geometry("400x335")
+        self.root.geometry("435x425")
         self.root.resizable(0, 0)
         self.root.title("BepInstaller - The Automatic BepInEX Installer by Spiral")
         self.root.iconbitmap("resources/bepinstaller.ico")
@@ -67,24 +67,60 @@ class InterfaceMenu:
 
         self.steam_frame = Frame(self.main_frame, background="#353738")
         self.nonsteam_frame = Frame(self.main_frame, background="#353738")
+        
+        self.bepinex_versions = self.get_bepinex_versions()
+        self.selected_version = StringVar(self.root)
+        self.selected_version.set(self.get_default_stable_version()) # Default to most recent stable version
+        
+        self.scripting_backend = "Mono"
 
         # Steam installation page
         self.create_steam_page(self.steam_frame)
+        
         # Non-Steam installation page
         self.create_nonsteam_page(self.nonsteam_frame)
 
-        # Show steam frame by default
+        # Show steam frame by  default
         self.current_frame = self.steam_frame
         self.show_frame(self.current_frame)
 
-        # Create toggle button
+        # Create togglle button
         nav_frame = Frame(self.root, background="#353738")
         nav_frame.pack()
         self.toggle_button = Button(nav_frame, text="Non-Steam Installation", bg="#353738", fg="#edf2f4",
                                     command=self.toggle_installation_mode)
         self.toggle_button.grid(row=0, column=0, padx=10, pady=(10, 10))
-
+        
         self.root.mainloop()
+    
+    def get_bepinex_versions(self):
+        releases = []
+        url = "https://api.github.com/repos/BepInEx/BepInEx/releases"
+        
+        while url:
+            response = requests.get(url)
+            if response.status_code == 200:
+                releases_data = response.json()
+                releases.extend([release['tag_name'] for release in releases_data])
+                
+                # Check if there is a "next" page
+                if 'next' in response.links:
+                    url = response.links['next']['url']
+                else:
+                    url = None
+            else:
+                print(f"Error fetching versions: {response.status_code}")
+                return ["5.4.21", "6.0.0-pre.1", "6.0.0-pre.2"]  # Default falback
+
+        return releases
+    
+    def get_default_stable_version(self):
+        stable_versions = [version for version in self.bepinex_versions if 'pre' not in version and 'RC' not in version] # Filters out any pre or RC(release canidate) versions
+        
+        if stable_versions:
+            return stable_versions[0]  # Return the latest stable version
+        else:
+            return self.bepinex_versions[0]  # Fallback 
 
     def show_frame(self, frame_to_show):
         """Show only the selected frame and hide the others."""
@@ -109,6 +145,11 @@ class InterfaceMenu:
         heading = Label(frame, text="Steam Installer", font=("Calibri", 18), background="#353738", fg="#edf2f4")
         heading.grid(row=0, column=0, columnspan=2, pady=10, sticky="ew")
         
+        Label(frame, text="Select BepInEx Version:", background="#353738", fg="#edf2f4", font=("Calibri", 12)).grid(row=5, column=0, columnspan=2, padx=10, pady=(5, 5), sticky="ew")
+        version_dropdown = OptionMenu(frame, self.selected_version, *self.bepinex_versions)
+        version_dropdown.config(background="#353738", fg="#edf2f4", font=("Calibri", 12))
+        version_dropdown.grid(row=6, column=0, columnspan=2, padx=10, pady=(5, 20), sticky="")
+
         Label(frame, background="#353738", fg="#edf2f4", font=("Calibri", 12), text="Game Name Folder").grid(row=1, column=0, columnspan=2, padx=10, pady=(10, 5), sticky="ew")
         self.game_folder_entry = Entry(frame, background="#353738", fg="#edf2f4", font=("Calibri", 12))
         self.game_folder_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=(0, 20), sticky="ew")
@@ -143,6 +184,8 @@ class InterfaceMenu:
         def install_bepinex():
             foldername = self.game_folder_entry.get()
             steam_dir = self.steam_dir_entry.get()
+            version = self.selected_version.get()
+            scripting_backend = detect_scripting_backend(os.path.join(steam_dir, foldername))
 
             if not foldername.strip():
                 messagebox.showerror("Error", "Game Name Folder cannot be empty.")
@@ -153,7 +196,7 @@ class InterfaceMenu:
 
             if steam_dir and os.path.isdir(os.path.join(steam_dir, foldername)):
                 architecture = get_system_architecture()
-                download_url = get_bepinex_download_url(architecture)
+                download_url = get_bepinex_download_url(architecture, version, scripting_backend)
                 dest = os.path.join(os.getcwd(), "resources/BepInEx.zip")
                 if not os.path.isdir(os.path.join(steam_dir, foldername, "BepInEx")):
                     download_file(download_url, dest)
@@ -169,8 +212,7 @@ class InterfaceMenu:
                 messagebox.showerror("Error", "Invalid directory: The game folder does not exist.")
                 
         
-
-        Button(frame, text="Install BepInEX", bg="#353738", fg="#edf2f4", command=install_bepinex).grid(row=5, column=0, columnspan=2, padx=10, pady=(10, 20))
+        Button(frame, text="Install BepInEX", bg="#353738", fg="#edf2f4", command=install_bepinex).grid(row=7, column=0, columnspan=2, padx=10, pady=(10, 20))
 
         # Configure grid columns to ensure centering
         frame.grid_columnconfigure(0, weight=1)
@@ -178,6 +220,8 @@ class InterfaceMenu:
         frame.grid_rowconfigure(2, weight=1)
         frame.grid_rowconfigure(4, weight=1)
         frame.grid_rowconfigure(5, weight=1)
+        frame.grid_rowconfigure(6, weight=1)
+        frame.grid_rowconfigure(7, weight=1)
 
 
     def create_nonsteam_page(self, parent):
@@ -185,24 +229,26 @@ class InterfaceMenu:
         frame.pack(fill="both", expand=True)
 
         heading = Label(frame, text="Non-Steam Installer", font=("Calibri", 18), background="#353738", fg="#edf2f4")
-        heading.grid(row=0, column=0, columnspan=2, pady=15, sticky="ew")
+        heading.grid(row=0, column=0, columnspan=2, pady=15, sticky="ews")
 
         Label(frame, background="#353738", fg="#edf2f4", font=("Calibri", 12), text="Game Directory").grid(row=1, column=0, columnspan=2, padx=10, sticky="ews")
         self.game_dir_entry = Entry(frame, background="#353738", fg="#edf2f4", font=("Calibri", 12))
-        self.game_dir_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=10, sticky="ewn")
+        self.game_dir_entry.grid(row=2, column=0, columnspan=2, padx=10, pady=0, sticky="ewn")
 
         Tooltip(self.game_dir_entry, "Enter the full directory path for the game installation.")
 
         def install_bepinex_nonsteam():
             foldername = self.game_dir_entry.get()
-
+            version = self.selected_version.get()
+            scripting_backend = detect_scripting_backend(foldername)
+            
             if not foldername.strip():
                 messagebox.showerror("Error", "Game Directory cannot be empty.")
                 return
 
             if os.path.isdir(foldername):
                 architecture = get_system_architecture()
-                download_url = get_bepinex_download_url(architecture)
+                download_url = get_bepinex_download_url(architecture, version,scripting_backend)
                 dest = os.path.join(os.getcwd(), "resources/BepInEx.zip")
                 if not os.path.isdir(os.path.join(foldername, "BepInEx")):
                     download_file(download_url, dest)
@@ -233,20 +279,21 @@ class InterfaceMenu:
                 messagebox.showerror("Error", "Path " + os.path.join(steam_path, "steamapps", "common") + " doesn't exist")
             return None
 
-        # Create a label to explain the "Open Folder" button
-        # open_folder_label = Label(frame, text="Click 'Open Folder' to select your game directory", background="#353738", fg="#edf2f4", font=("Calibri", 10))
-        # open_folder_label.grid(row=3, column=0, columnspan=2, padx=10, pady=(10, 5), sticky="ew")
-
         # Place the "Open Folder" button directly below the explanatory label
         open_folder_btn = Button(frame, text="Open Folder", bg="#353738", fg="#edf2f4", command=open_folder)
-        open_folder_btn.grid(row=4, column=0, padx=10, pady=(5, 10), sticky="ewn")
+        open_folder_btn.grid(row=3, column=0, padx=10, pady=(0, 10), sticky="ew")
         Tooltip(open_folder_btn, "Click 'Open Folder' to select your game directory or type it in yourself")
+        
+        Label(frame, text="Select BepInEx Version:", background="#353738", fg="#edf2f4", font=("Calibri", 12)).grid(row=4, column=0, columnspan=2, padx=10, pady=(5, 5), sticky="ew")
+        version_dropdown = OptionMenu(frame, self.selected_version, *self.bepinex_versions)
+        version_dropdown.config(background="#353738", fg="#edf2f4", font=("Calibri", 12))
+        version_dropdown.grid(row=6, column=0, columnspan=2, padx=10, pady=(5, 70), sticky="")
 
         # Add an empty row for spacing
-        frame.grid_rowconfigure(5, minsize=20)
+        frame.grid_rowconfigure(9, minsize=20)
 
         # Place the "Install BepInEX" button below the spacing row
-        Button(frame, text="Install BepInEX", bg="#353738", fg="#edf2f4", command=install_bepinex_nonsteam).grid(row=6, column=0, columnspan=2, padx=10, pady=(5, 10))
+        Button(frame, text="Install BepInEX", bg="#353738", fg="#edf2f4", command=install_bepinex_nonsteam).grid(row=9, column=0, columnspan=2, padx=10, pady=(5, 50))
 
         # Configure grid columns to ensure centering
         frame.grid_columnconfigure(0, weight=1)
@@ -288,18 +335,91 @@ def get_system_architecture():
     architecture = platform.architecture()[0]
     return 'x64' if '64' in architecture else 'x86'
 
-def get_bepinex_download_url(architecture):
-    base_url = "https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.2/BepInEx"
+def generate_urls(base_url, architecture, version, os_system, scripting_backend):
+    nov = version.replace("v", "")
+    nop = scripting_backend.replace(".", "")
+    suffixes = [
+        f"{base_url}_{os_system.lower()}_{architecture}_{nov}.zip",
+        f"{base_url}_{os_system.lower()}_{architecture}_{nov}.0.zip",
+        f"{base_url}_{os_system.lower()}_{architecture}_{nov}.0.0.zip",
+        f"{base_url}_{architecture}_{nov}.zip",
+        f"{base_url}_{architecture}_{nov}.0.zip",
+        f"{base_url}_{architecture}_{nov}.0.0.zip",
+        f"{base_url}-{scripting_backend}-{os_system.lower()}-{architecture}-{nov}.zip",
+        f"{base_url}-{scripting_backend}-{os_system.lower()}-{architecture}-{nov}.0.zip",
+        f"{base_url}-{scripting_backend}-{os_system.lower()}-{architecture}-{nov}.0.0.zip",
+        f"{base_url}_{nop}_{architecture}_{nov}.zip",
+        f"{base_url}_{nop}_{architecture}_{nov}.0.zip",
+        f"{base_url}_{nop}_{architecture}_{nov}.0.0.zip",
+        f"{base_url}_{version}.zip"
+    ]
+    return suffixes
+
+def get_bepinex_download_url(architecture, version, scripting_backend):
+    base_url = f"https://github.com/BepInEx/BepInEx/releases/download/{version}/BepInEx"
     os_system = platform.system()
-    if os_system == "Windows":
-        return f"{base_url}_win_{architecture}_5.4.23.2.zip"
-    elif os_system == "Linux":
-        return f"{base_url}_linux_{architecture}_5.4.23.2.zip"
-    elif os_system == "Darwin":
-        return f"{base_url}_macos_x64_5.4.23.2.zip"
-    else:
+
+    if os_system not in ["Windows", "Linux", "Darwin"]:
         messagebox.showerror("Error", "Unsupported OS detected.")
         return None
+
+    os_system = "win" if os_system == "Windows" else "linux" if os_system == "Linux" else "macos_x64"
+    urls = generate_urls(base_url, architecture, version, os_system, scripting_backend)
+
+    for url in urls:
+        print(f"Testing URL: {url}")
+        try:
+            response = requests.head(url, allow_redirects=True)
+            if response.status_code == 200:
+                print(f"Valid URL found: {url}")
+                return url
+        except requests.RequestException as e:
+            print(f"Failed to retrieve URL: {url}, Error: {e}")
+
+    messagebox.showerror("Error", "No valid download URL found.")
+    return None
+
+def detect_scripting_backend(game_directory): # REQUIRED FOR VERSIONS 6.0.0 PRE 1 AND HIGHER
+    # List of known IL2CPP files
+    il2cpp_files = [
+        "GameAssembly.dll",
+        "global-metadata.dat",
+        "libil2cpp.so",
+        "GameAssembly.dll",
+        # Add other common IL2CPP files here
+    ]
+    
+    # List of known Mono files
+    mono_files = [
+        "Assembly-CSharp.dll",
+        "mono.dll",
+        "libmono.so",
+        # Add other common Mono files here
+    ]
+    data_folder = None
+    for folder in os.listdir(game_directory):
+        if folder.endswith('_Data') and os.path.isdir(os.path.join(game_directory, folder)):
+            data_folder = os.path.join(game_directory, folder)
+            break
+
+    if not data_folder:
+        return "No _Data folder found in the game directory"
+    
+    # Check for Mono files
+    for file in mono_files:
+        if os.path.isfile(os.path.join(data_folder, 'Managed', file)):
+            return "Unity.Mono"
+        elif os.path.isfile(os.path.join(game_directory, file)):
+            return "Unity.Mono"
+    
+    # Check for IL2CPP files
+    for file in il2cpp_files:
+        if os.path.isfile(os.path.join(data_folder, file)):
+            return "Unity.IL2CPP"
+        elif os.path.isfile(os.path.join(game_directory, file)):
+            return "Unity.IL2CPP"
+    
+    return None
 
 def download_file(url, dest_path):
     response = requests.get(url, stream=True)
